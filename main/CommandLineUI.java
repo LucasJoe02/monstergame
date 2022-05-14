0803c6b6f84c9739b3d9204a64f287269f50ce64
Lucas Redding <lre61@uclive.ac.nz>
2022-05-14 23:04:07 +1200

    completed UI setup function for starting a game

--- main/CommandLineUI.java
+++ main/CommandLineUI.java
@@ -2,6 +2,7 @@
  * 
  */
 package main;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher; 
 import java.util.regex.Pattern;
@@ -24,22 +25,22 @@ public class CommandLineUI {
 	}
 	
 	private boolean isValidName(String name) {
-		Pattern pattern = Pattern.compile("[a-zA-Z]");
+		Pattern pattern = Pattern.compile("[^a-zA-Z]");
 		Matcher matcher = pattern.matcher(name);
 		boolean valid = false;
-		if (matcher.find() && name.length() <= 15 && name.length() >= 3) {
+		if (!matcher.find() && name.length() <= 15 && name.length() >= 3) {
 			valid = true;
 		}
 		return valid;
 	}
 	
-	private boolean isValidDays(String days) {
+	private boolean isInputNumValid(String num,int lower,int upper) {
 		Pattern pattern = Pattern.compile("[0-9]");
-		Matcher matcher = pattern.matcher(days);
+		Matcher matcher = pattern.matcher(num);
 		boolean valid = false;
 		try {
-			int daysInt = Integer.parseInt(days);
-			if (matcher.find() && daysInt >= 5 && daysInt <= 15) {
+			int daysInt = Integer.parseInt(num);
+			if (matcher.find() && daysInt >= lower && daysInt <= upper) {
 				valid = true;
 			}
 		}catch (Exception e) {
@@ -48,7 +49,7 @@ public class CommandLineUI {
 		return valid;
 	}
 	
-	public void setup() {
+	public String inputName() {
 		System.out.print("Enter Username: ");
 		String userName = scanner.nextLine();
 		while(!isValidName(userName)) {
@@ -57,14 +58,68 @@ public class CommandLineUI {
 			System.out.print("Enter Username: ");
 			userName = scanner.nextLine();
 		}
-		
-		System.out.println("Hi! "+userName+".");
+		return userName;
+	}
+	
+	public int inputDays() {
+		int days;
 		System.out.println("How many days would you like the game to last?");
-		String days;
+		String daysString;
 		do {
-			System.out.println("(Must be between 5 and 15 days): ");
-			days = scanner.nextLine();
-		}while(!isValidDays(days));
+			System.out.print("(Must be between 5 and 15 days): ");
+			daysString = scanner.nextLine();
+		}while(!isInputNumValid(daysString,5,15));
+		days = Integer.parseInt(daysString);
+		return days;
+	}
+	
+	public Monster chooseStartMonster() {
+		Monster chosenMonster;
+		System.out.println("\nBelow is a list of available monsters for your team.");
+		System.out.println("Above each monster is it's number.");
+		System.out.println("Enter the number of the monster you want to be your starter below.");
+		MonsterCreator monsterCreator = new MonsterCreator();
+		ArrayList<Monster> availableMonsters = new ArrayList<Monster>();
+		for (int i = 0; i < 3; i++) {
+			System.out.println();
+			System.out.println(i);
+			availableMonsters.add(monsterCreator.createCommon());
+			System.out.println(availableMonsters.get(i));
+		}
+		String monsterIndexString;
+		do {
+			System.out.print("(Enter number of chosen monster): ");
+			monsterIndexString = scanner.nextLine();
+		}while(!isInputNumValid(monsterIndexString,0,2));
+		int monsterIndex = Integer.parseInt(monsterIndexString);
+		chosenMonster = availableMonsters.get(monsterIndex);
+		return chosenMonster;
+	}
+	
+	public int chooseDifficulty() {
+		int difficulty;
+		String difficultyInputString;
+		do {
+			System.out.print("Enter a difficulty level from 1 to 3 (1-Easy,2-Medium,3-Hard): ");
+			difficultyInputString = scanner.nextLine();
+		}while(!isInputNumValid(difficultyInputString,1,3));
+		difficulty = Integer.parseInt(difficultyInputString);
+		return difficulty;
+	}
+	
+	public void setup() {
+		String userName;
+		int days;
+		Monster chosenMonster;
+		int difficulty;
+		
+		userName = inputName();
+		System.out.println("Hi! "+userName+".");
+		days = inputDays();
+		chosenMonster = chooseStartMonster();
+		difficulty = chooseDifficulty();
+		
+		
 	}
 
 	/**
--- main/GameEnvironment.java
+++ main/GameEnvironment.java
@@ -10,19 +10,60 @@ package main;
  */
 public class GameEnvironment {
 
+	private Player player;
+	private Shop shop;
+	private Arena arena;
 	private int day;
-//	private Player player;
-//	private Shop shop;
-//	private Arena arena;
+	private int maxDays;
+	private int difficulty;
 	
 	/**
 	 * 
 	 */
 	public GameEnvironment() {
-//		player = new Player();
-//		day = 1;
-//		shop = new Shop();
-//		arena = new Arena();
+		day = 1;
+		shop = new Shop();
+		arena = new Arena();
+	}
+	
+	public Player getPlayer() {
+		return player;
+	}
+	
+	public void setPlayer() {
+		player = new Player();
+	}
+	
+	public Shop getShop() {
+		return shop;
+	}
+	
+	public Arena getArena() {
+		return arena;
+	}
+	
+	public int getDay() {
+		return day;
+	}
+	
+	public void increaseDay() {
+		day += 1;
+	}
+	
+	public int getMaxDays() {
+		return maxDays;
+	}
+	
+	public void setMaxDays(int maxDays) {
+		this.maxDays = maxDays;
+	}
+	
+	public int getDifficulty() {
+		return difficulty;
+	}
+	
+	public void setDifficulty(int difficulty) {
+		this.difficulty = difficulty;
 	}
 
 }
