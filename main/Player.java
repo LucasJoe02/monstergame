321b03c57524fdb9b0bd8f5ed6d5efcc20b65748
Lucas Redding <lre61@uclive.ac.nz>
2022-05-17 16:02:32 +1200

    Wrote full javadoc comments for CommandLineUI, GameEnvironment, Player, Purchasable and squad classes

--- main/Player.java
+++ main/Player.java
@@ -18,15 +18,31 @@ import java.util.ArrayList;
  */
 public class Player {
 	
-	String name;
-	int gold;
-	int points;
-	Squad squad;
-	ArrayList<Item> items;
+	/** 
+	 * The name of this player.
+	 */
+	private String name;
+	/** 
+	 * The amount of gold this player has.
+	 */
+	private int gold;
+	/** 
+	 * The amount of points this player has.
+	 */
+	private int points;
+	/** 
+	 * The Squad of Monsters associated with this player.
+	 */
+	private Squad squad;
+	/** 
+	 * The list of items that this player has.
+	 */
+	private ArrayList<Item> items;
 
 
 	/**
-	 * 
+	 * The default constructor for a player.
+	 * Instantiates the variables of this player to their default values.
 	 */
 	public Player() {
 		points = 0;
@@ -35,42 +51,82 @@ public class Player {
 		items = new ArrayList<Item>();
 	}
 	
+	/** 
+	 * Gets the name of this player.
+	 * @return the name of this player.
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/** 
+	 * Sets the name of this player.
+	 * @param name the new name for this player.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/** 
+	 * Gets the amount of gold of this player.
+	 * @return the amount of gold of this player.
+	 */
 	public int getGold() {
 		return gold;
 	}
 	
+	/** 
+	 * Increases the gold of this player by a given amount.
+	 * @param gold the amount of gold to increase by.
+	 */
 	public void increaseGold(int gold) {
 		this.gold += gold;
 	}
 	
+	/** 
+	 * Gets the points of this player.
+	 * @return the points of this player.
+	 */
 	public int getPoints() {
 		return points;
 	}
 	
+	/** 
+	 * Increase the points of this player by a given amount.
+	 * @param addPoints the amount of points to add to this player's points.
+	 */
 	public void increasePoints(int addPoints) {
 		points += addPoints;
 	}
 	
+	/** 
+	 * Gets the squad of this player.
+	 * @return the squad of this player.
+	 */
 	public Squad getSquad() {
 		return squad;
 	}
 	
+	/** 
+	 * Gets the list of items of this player.
+	 * @return list of items of this player.
+	 */
 	public ArrayList<Item> getItems(){
 		return items;
 	}
 	
+	/** 
+	 * Add and item to the list of items of this player.
+	 * @param newItem the item to add to the list of items of this player.
+	 */
 	public void addItem(Item newItem) {
 		items.add(newItem);
 	}
 	
+	/** 
+	 * Returns the items of this player as a string with the indexes of the item.
+	 * @return string of items and indexes.
+	 */
 	public String itemsString() {
 		String itemsString = new String();
 		for (int index = 0; index < items.size(); index++) {
@@ -79,18 +135,14 @@ public class Player {
 		return itemsString;
 	}
 	
+	/** 
+	 * Removes the item at a given index from this player's list of items and increases the player's gold.
+	 * The gold gained is taken from the resell price of the item being sold.
+	 * @param itemIndex the index of the item, in the plyaer's list of items, being sold.
+	 */
 	public void sellItem(int itemIndex) {
 		increaseGold(items.get(itemIndex).getResellPrice());
 		items.remove(itemIndex);
 	}
-
-//	public static void main(String[] args) {
-//		Player player = new Player();
-//		Item item = new Item();
-//		player.addItem(item);
-//		player.addItem(item);
-//		System.out.println(player.itemsString());
-//
-//	}
 
 }
--- main/Squad.java
+++ main/Squad.java
@@ -16,19 +16,30 @@ import java.util.ArrayList;
  */
 public class Squad {
 
+	/** 
+	 * A list of Monsters associated with this squad.
+	 */
 	ArrayList<Monster> monsters;
 	
 	/**
-	 * 
+	 * Instantiates the variables for this Squad.
 	 */
 	public Squad() {
 		monsters = new ArrayList<Monster>();
 	}
 	
+	/** 
+	 * Gets the list of Monsters in this Squad.
+	 * @return list of Monsters in this squad.
+	 */
 	public ArrayList<Monster> getMonsters(){
 		return monsters;
 	}
 	
+	/** 
+	 * Adds a given Monster to this squad.
+	 * @param newMonster the new Monster to be added to this squad.
+	 */
 	public void addMonster(Monster newMonster) {
 		monsters.add(newMonster);
 	}
